fdff379bb49105a6485f25a15eee8eb7b489c312
mrk-oss <gkmarko86@gmail.com>
2025-10-03 19:10:11 +0300

    Exported richTextHasContent Util

--- index.ts
+++ index.ts
@@ -2,3 +2,4 @@ export * from './richtext';
 export * from './types';
 export * from "./richTextRender";
 export { default as RichTextRender } from "./RichTextRender.astro";
+export { richTextHasContent } from "./richTextHasContent";
---
+++ richTextHasContent.ts
@@ -0,0 +1,14 @@
+export const richTextHasContent = (richText) => {
+    try {
+        let content = richText?.content || [];
+        for (let item of content) {
+            if (Array.isArray(item.content) && item.content?.length > 0) {
+                return true;
+            }
+        }
+        return false;
+    } catch (error) {
+        console.error("Invalid JSON input:", error);
+        return false;
+    }
+};
